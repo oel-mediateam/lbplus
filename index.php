ae9dbd2b1f7e4572573ccf3505acf451784d5b69
Ethan Lin <ethan.lin@uwex.edu>
2015-03-20 11:35:03 -0500

    Updated the index page with view code

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
+<?php
+
+    // 1 is lbplus view; 0 is score view
+
+    $request_view = 1;
+
+    if ( $request_view ) {
+
+        $view = 'includes/views/lbplus_view.php';
+
+    } else {
+
+        $view = 'includes/views/score_view.php';
+
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>LB+</title>
     </head>
     <body>
-        <?php include_once 'includes/views/lbplus_view.php'; ?>
+        <?php include_once $view; ?>
     </body>
 </html>
