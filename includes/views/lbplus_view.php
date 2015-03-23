20a6531957e0692484e0e59b58e58483b5d81307
Ethan Lin <ethan.lin@uwex.edu>
2015-03-23 18:11:45 -0500

    Front-end started

--- includes/views/lbplus_view.php
+++ includes/views/lbplus_view.php
@@ -1,1 +1,23 @@
-<h1>LB+ View</h1>
+<h1>LiveButton+ Presents</h1>
+<div class="lbplus_interaction_wrapper">
+    <div class="lbplus_media">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/GCRgfImEqaY?rel=0" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="lbplus_actions">
+
+        <h4>Actions</h4>
+
+        <div class="btn">
+            <span class="icon"></span>
+            <span class="action_name">OMG! OMG! OMG!</span>
+            <span class="cooldown"><span class="progress" style="width: 50px;"></span></span>
+        </div>
+
+        <div class="btn">
+            <span class="icon"></span>
+            <span class="action_name long">Shut up!<br />... and take my money!</span>
+            <span class="cooldown"><span class="progress" style="width: 125px;"></span></span>
+        </div>
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+    // DEV ONLY
     // 1 is lbplus view; 0 is score view
 
     $request_view = 1;
@@ -14,15 +15,26 @@
 
     }
 
+
 ?>
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>LB+</title>
+        <link href="css/lbplus.css" rel="stylesheet" type="text/css" media="all" />
     </head>
     <body>
-        <?php include_once $view; ?>
+        <main class="lbplus_wrapper" role="main">
+            <section class="lbplus_view">
+                <?php include_once $view; ?>
+            </section>
+            <nav class="lbplus_controls">
+
+            </nav>
+        </main>
     </body>
+    <script src="scripts/jquery.js" type="text/javascript"></script>
+    <script src="scripts/min/lbplus.js" type="text/javascript"></script>
 </html>
