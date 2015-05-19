57441ec8921d479a227d1a7ccb27f66f51ea5993
Ethan Lin <ethan.lin@uwex.edu>
2015-05-19 09:14:01 -0500

    renamed the file

---
+++ includes/student_input.php
@@ -0,0 +1,23 @@
+<?php
+
+    if ( !isset( $$_POST['student'] ) ) {
+
+        header( 'HTTP/1.0 404 File Not Found', 404 );
+        exit('Error 404 - Page Not Found');
+
+    } else {
+
+        $inputs = $_POST['student'];
+
+/*
+        foreach( $inputs as $data ) {
+
+            echo $data['name'] . '<br />';
+
+        }
+*/
+
+    }
+
+
+?>
