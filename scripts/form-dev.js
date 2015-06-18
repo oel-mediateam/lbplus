3d24b35319a224fafee4aee00cff6b3c53cb9e80
Ethan Lin <ethan.lin@uwex.edu>
2015-06-17 19:16:26 -0500

    modified form.js to copy over class on select input element

--- scripts/form-dev.js
+++ scripts/form-dev.js
@@ -3,13 +3,17 @@ $( function() {
     $( 'select' ).each( function() {
     
         var $this = $( this );
+        var opened = false;
         var numberOfOptions = $( this ).children( 'option' ).length;
+        var existingClasses = $this.attr('class');
         
         $this.addClass( 'select-hidden' ); 
         $this.wrap( '<div class="select"></div>' );
         $this.after( '<div class="select-styled"></div> ');
     
         var styledSelect = $this.next('div.select-styled');
+        
+        styledSelect.addClass( existingClasses );
         styledSelect.text( $this.children( 'option' ).eq( 0 ).text() );
       
         var list = $( '<ul />', {
@@ -22,7 +26,7 @@ $( function() {
             
             $('<li />', {
                 text: $this.children( 'option' ).eq( i ).text(),
-                rel: $this.children( 'option' ).eq( i ).val()
+                rel: $this.children( 'option' ).eq( i ).val(),
             }).appendTo( list );
             
         }
@@ -33,13 +37,25 @@ $( function() {
             
             e.stopPropagation();
             
-            $( 'div.select-styled.active' ).each( function() {
+            if ( opened ) {
                 
-                $( this ).removeClass( 'active' ).next( 'ul.select-options' ).hide();
+                styledSelect.removeClass( 'active' );
+                list.hide();
+                opened = false;
                 
-            } );
-            
-            $( this ).toggleClass( 'active' ).next( 'ul.select-options' ).toggle();
+            } else {
+                
+                $( 'div.select-styled.active' ).each( function() {
+                
+                    $( this ).removeClass( 'active' ).next( 'ul.select-options' ).hide();
+                    
+                } );
+                
+                $( this ).toggleClass( 'active' ).next( 'ul.select-options' ).toggle();
+                
+                opened = true;
+                
+            }
             
         } );
       
@@ -49,13 +65,15 @@ $( function() {
             styledSelect.text( $( this ).text() ).removeClass( 'active' );
             $this.val( $( this ).attr( 'rel' ) );
             list.hide();
+            opened = false;
             
         } );
       
-        $( document ).click( function() {
-            
-            styledSelect.removeClass( 'active' );
-            list.hide();
+        $( document, styledSelect ).click( function() {
+                
+                styledSelect.removeClass( 'active' );
+                list.hide();
+                opened = false;
             
         } );
     
